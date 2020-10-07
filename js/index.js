14dbd9409d6d891e4adee49178f8176ef29787c1
AlexDyatlov <aleks78.gooogle@gmail.com>
2020-10-07 23:18:23 +0300

    homework-1

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-const modal = $.modal()
+const modal = $.modal({
+  title: 'Modal title',
+  closable: true,
+  content: `
+    <h4>Modal is working</h4>
+    <p>Lorem ipsum dolor sit.</p>
+  `,
+  width: '400px'
+})
--- plugins/modal.js
+++ plugins/modal.js
@@ -1,16 +1,16 @@
 function _createModal(options) {
+  const defaultWidth = '600px'
   const modal = document.createElement('div')
   modal.classList.add('modal')
   modal.insertAdjacentHTML('afterbegin', `
-  <div class="modal-overlay">
-    <div class="modal-window">
+  <div class="modal-overlay" data-close="true">
+    <div class="modal-window" style="width: ${options.width || defaultWidth}">
       <div class="modal-header">
-        <span class="modal-title">Modal title</span>
-        <span class="modal-close">&times;</span>
+        <span class="modal-title">${options.title || 'Тайтл'}</span>
+        ${options.closable ? `<span class="modal-close" data-close="true">&times;</span>` : ''}
       </div>
       <div class="modal-body">
-        <p>Lorem ipsum dolor sit.</p>
-        <p>Lorem ipsum dolor sit.</p>
+        ${options.content || ''}
       </div>
       <div class="modal-footer">
         <button>Ok</button>
@@ -21,18 +21,35 @@ function _createModal(options) {
   `)
   document.body.appendChild(modal)
   return modal
-}
+};
+
+/* ДЗ
+*title: string +
+*closable +
+* content: string +
+*width: string ('400px') +
+*destroy(): void
+ закрытие на крестик и фон +
+ ------------
+  setContent(html: string): void | PUBLIC
+  onClose(): void
+  onOpne(): void
+  beforeClose(): boolean
+  --------
+  animate.css
+*/
 
 $.modal = function(options) {
   const ANIMATION_SPEED = 200;
   const $modal = _createModal(options); 
   let closing = false
+  // let content = document.querySelector('.modal-window');
 
-
-  return {
+  const modal = {
     open() {
-      !closing && $modal.classList.add('open')
-    },
+      !closing && $modal.classList.add('open');
+      // content.style.width = '400px';
+  },
     close() {
       closing = true
       $modal.classList.remove('open')
@@ -42,6 +59,15 @@ $.modal = function(options) {
         closing = false
       }, ANIMATION_SPEED)
     },
-    destroy() {}
   }
+
+  $modal.addEventListener('click', e => {
+    console.log('Clicked', e.target.dataset.close);
+    if (e.target.dataset.close) {
+      modal.close()
+    }
+  })
+
+    return modal
+   
 }
